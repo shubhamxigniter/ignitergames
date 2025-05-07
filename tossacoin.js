d890590f6e65914454b0430fc7ecfe7e87a508ec
shubhamxigniter <shubhamzxz1213@gmail.com>
2025-05-07 13:47:07 +0530

    Add files via upload

---
+++ tossacoin.js
@@ -0,0 +1,19 @@
+const tossbutton = document.getElementById("tossButton");
+const result = document.getElementById("result");
+
+tossbutton.addEventListener("click", tossCoin);
+
+function tossCoin() {
+  const randomNumber = Math.random();
+  if (randomNumber < 0.5) {
+    result.innerHTML = "Loaeding...";
+    setTimeout(() => {
+      result.innerHTML = "Heads";
+    }, 1000);
+  } else {
+    result.innerHTML = "Loading...";
+    setTimeout(() => {
+      result.innerHTML = "Tails";
+    }, 1000);
+  }
+}
